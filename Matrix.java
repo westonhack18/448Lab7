d126eceb1fe799bb3fac1b8d3bf4325166f68091
Weston Hack <whack48@1005D-20.eecs.ku.edu>
2017-03-16 11:25:34 -0500

    Created determinant method in matrix class

--- Matrix.java
+++ Matrix.java
@@ -28,6 +28,29 @@ public class Matrix {
     
     
     }
+    
+    //Method that returns the determinant of the matrix
+    public double determinant() {
+    
+        double det = 0.0;
+        
+        if (size == 1) {
+            det = mdata[0][0];
+        
+        }
+        else if (size == 2) {
+            det = mdata[0][0] * mdata[1][1] - mdata[0][1] * mdata[1][0];
+        
+        }
+        else {
+            for (int i = 0; i < size; i++) {
+                det += Math.pow(-1.0, (double)i) * mdata[0][i] * getSubMatrix(0,i).determinant();
+            }
+        
+        }
+        
+        return det;
+        
 
 
 
